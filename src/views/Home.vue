23abc299bda86d67296cc9ea64b8d5759fcd2fba
yxc <1379636828@qq.com>
2020-10-14 11:06:01 +0800

    仿真平台初始项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app-wrapper">
+    <el-container class="container">
+      <el-header class="header">
+        <div class="title">物联网仿真平台</div>
+      </el-header>
+      <el-container>
+        <el-aside style="width:200px;height:100%;">
+          <el-menu
+            default-active="2"
+            style="height:100%"
+            @open="handleOpen"
+            @close="handleClose"
+            background-color="#304156"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-menu-item index="0">
+                <template slot="title">
+                  <i class="el-icon-view"></i>
+                  <span>首页</span>
+                </template>
+              </el-menu-item>
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-user"></i>
+                <span>用户管理</span>
+              </template>
+              <el-submenu index="1-1">
+                <template slot="title">选项1</template>
+                <el-menu-item index="1-1-1">选项1</el-menu-item>
+                <el-menu-item index="1-1-2">选项2</el-menu-item>
+              </el-submenu>
+              <el-submenu index="1-2">
+                <template slot="title">选项2</template>
+                <el-menu-item index="1-2-1">选项1</el-menu-item>
+              </el-submenu>
+              <el-submenu index="1-3">
+                <template slot="title">选项3</template>
+                <el-menu-item index="1-3-1">选项1</el-menu-item>
+              </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+              <i class="el-icon-menu"></i>
+              <span slot="title">仿真</span>
+            </el-menu-item>
+            <el-menu-item index="3">
+              <i class="el-icon-setting"></i>
+              <span slot="title">系统设置</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main class="el-main">
+          <simulation></simulation>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Simulation from './Simulation.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components:{
+    Simulation
+  },
+  data() {
+    return {
+
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+
   }
-}
+};
 </script>
+
+<style scoped>
+
+.app-wrapper{
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+.app-wrapper .container {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
+}
+.header {
+  background-color:#3c8dbc;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-sizing: border-box;
+}
+.header .title {
+  font-size: 20px;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  color: white;
+}
+.el-main{
+  padding-left:10px;
+  padding-top:10px;
+  padding-bottom: 0px;
+  width: 100%;
+  height:100%;
+}
+</style>
---
+++ src/views/Simulation.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="main-container">
+    <div class="panel">
+      <el-collapse v-model="activeNames" @change="handleChange">
+        <el-collapse-item title="环境部件" name="1">
+          <div>
+            <!-- <el-image
+                    style="width: 50px; height: 50px"
+                    :src="require('@/assets/temp.png')"
+                  ></el-image> -->
+            一个图标 图标下面加一行字，鼠标放在上面显示
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="容器部件" name="2">
+          <div>路灯，建筑，正方体，长方体，圆柱，圆锥，球</div>
+        </el-collapse-item>
+        <el-collapse-item title="传感器部件" name="3">
+          <el-image
+            style="width: 50px; height: 50px"
+            :src="require('@/assets/temp.png')"
+          ></el-image>
+          <div>
+            温度，光照，速度，湿度，红外，距离，烟雾，天气（？），电流电压
+          </div>
+        </el-collapse-item>
+        <el-collapse-item title="其他部件" name="4">
+          <div>红绿灯，人，车辆，道路，灯泡</div>
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+    <div style="float: left; margin-left: 20px">
+      <canvas
+        id="canvas"
+        width="600"
+        height="600"
+        ref="canvas"
+        style="border: 1px solid #ccc"
+      ></canvas>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Simulation",
+  data() {
+    return {
+      mapWidth: "600",
+      mapHeight: "600",
+      gridWidth: "20",
+      gridHeight: "20",
+      // canvas: "",
+      ctx: {},
+    };
+  },
+  mounted() {
+    this.initGrid();
+  },
+  methods: {
+    initGrid() {
+      var canvas = this.$refs.canvas;
+      this.ctx = canvas.getContext("2d");
+      for (let i = 0; i < this.mapHeight / this.gridHeight; i++) {
+        for (let j = 0; j < this.mapWidth / this.gridWidth; j++) {
+          this.ctx.strokeStyle = "#E0E0E0";
+          this.ctx.strokeRect(
+            j * this.gridWidth,
+            i * this.gridHeight,
+            this.gridWidth,
+            this.gridHeight
+          );
+        }
+      }
+      // ctx.beginPath();
+      // ctx.fillStyle = "red";
+      // ctx.arc(a, b, r, 0, Math.PI * 2);
+      // ctx.fill();
+    }
+  }
+};
+</script>
+
+<style scoped>
+.main-container{
+  width: 100%;
+  height: 100%
+}
+.panel{
+  width: 200px;
+  border: 1px solid #ccc;
+  float: left;
+  padding-left: 5px;
+  padding-right: 5px;
+}
+</style>
